6019b100aced787dbf2672eb1eef2b9b3bb7c319
mijail <mijail86@gmail.com>
2024-10-20 00:36:53 -0500

    fix: improving todoheader and using react children and react cloneElement

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./TodoCounter.css";
 
-function TodoCounter({ completedTodos, totalTodos }) {
+function TodoCounter({ completedTodos, totalTodos, loading }) {
   return completedTodos && completedTodos === totalTodos ? (
     <h1 className="TodoCounter">Felicidades haz completado tus TODOs</h1>
   ) : (
-    <h1 className="TodoCounter">
+    <h1 className={`TodoCounter ${!!loading && "TodoCounter--loading"}`}>
       Has completado <span>{completedTodos}</span> de <span>{totalTodos}</span>{" "}
       TODOs
     </h1>
