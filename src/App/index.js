d7fa46fa6ab82086983231978a967413791e2b89
mijail <mijail86@gmail.com>
2024-10-19 23:50:30 -0500

    feat: improving render props and render function

--- src/App/index.js
+++ src/App/index.js
@@ -39,9 +39,14 @@ function App() {
         error={error}
         loading={loading}
         searchedTodos={searchedTodos}
+        totalTodos={totalTodos}
+        searchText={searchValue}
         onError={() => <TodosError />}
         onLoading={() => <TodosLoading />}
         onEmpty={() => <EmptyTodos />}
+        onEmptySearchResults={(searchText) => (
+          <p>No hay resultados para {searchText}</p>
+        )}
         render={(todo) => (
           <TodoItem
             key={todo.text}
@@ -51,13 +56,23 @@ function App() {
             onDelete={() => deleteTodo(todo.text)}
           />
         )}
-      />
+      >
+        {/* {(todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )} */}
+      </TodoList>
 
       <CreateTodoButton setOpenModal={setOpenModal} />
 
       {openModal && (
         <Modal>
-          <TodoForm setOpenModal={setOpenModal} />
+          <TodoForm setOpenModal={setOpenModal} addTodo={addTodo} />
         </Modal>
       )}
     </>
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,14 +1,20 @@
 import "./TodoList.css";
 
 function TodoList(props) {
+  const renderFunc = props.children || props.render;
+
   return (
     <section className="TodoList-container">
       {props.error && props.onError()}
       {props.loading && props.onLoading()}
-      {!props.loading && !props.searchedTodos.length && props.onEmpty()}
-      {props.searchedTodos.map(props.render)}
 
-      <ul className="TodoList">{props.children}</ul>
+      {!props.loading && !props.totalTodos && props.onEmpty()}
+
+      {!!props.totalTodos &&
+        !props.searchedTodos.length &&
+        props.onEmptySearchResults(props.searchText)}
+
+      {props.searchedTodos.map(renderFunc)}
     </section>
   );
 }
